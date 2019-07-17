cb075fd34b368404f721d2afb43d8f2346f381ed
404errors <nguoiviet95@gmail.com>
2019-07-18 06:35:20 +0700

    more about javascript

--- scripts/script.js
+++ scripts/script.js
@@ -8,9 +8,36 @@ function generateHeader(firstPart, secondPart) {
 	return result;
 
 }
+
+let myImage=document.querySelector('#myImg');
+myImage.onclick = function() {
+	let srcImg = myImage.getAttribute('src');
+	if (srcImg === 'firefox.webp') {
+		myImage.setAttribute('src','images/scene1.jpg');
+	} else {
+		myImage.setAttribute('src','firefox.webp');
+	}
+}
+let changeUserBtn =  document.querySelector('#changeUserBtn');
+let mainHeader = document.querySelector('h1');
+function setUserName() {
+	let inputName = prompt('Pls enter you name');
+	localStorage.setItem('inputName', inputName);
+	mainHeader.textContent= 'Mozilla is cool, ' + inputName;
+}
+if (!localStorage.getItem('inputName')) {
+	setUserName();
+} else {
+	let storedName = localStorage.getItem('inputName');
+	mainHeader.textContent = 'Mozilla is cool, ' + storedName;
+}
+/*
+function alertMe(string) {
+	alert('onclick attribute from ' + string );
+}
 let allSite = document.querySelector('img').onclick = function() {
 	alert('Stop poking me');
 }
-function alertMe() {
-	alert('onclick attribute');
-}
+let all = document.querySelector('img[src="firefox.webp"]').onclick= function() {alertMe('firefox');}
+//let another = document.querySelector('li').onclick=alertMe('property selector');
+*/
